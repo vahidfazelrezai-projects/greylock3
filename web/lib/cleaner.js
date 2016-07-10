c248b82175a7203a9800c1704d4eae50fcef3463
kimberli <kimberlizhong@gmail.com>
2016-07-10 10:29:58 -0700

    horseman kind of working

--- web/lib/cleaner.js
+++ web/lib/cleaner.js
@@ -56,6 +56,7 @@ returnBase = function(parts) {
 
 cleaner.clean = function (html, url, cb) {
     var $ = cheerio.load(html);
+    console.log($)
     images = $('img');
     processImage = function(i) {
         if (i === images.length) {
@@ -83,35 +84,38 @@ cleaner.clean = function (html, url, cb) {
                 link = links[j];
                 href = $(link).attr('href');
                 console.log(href);
-                if (href.indexOf(".css") > -1) {
-                    console.log("THIS IS A CSS FILE");
-                    parts = PARSE_URL.exec(url);
-                    base = returnBase(parts);
-                    if (base.indexOf("http") !== 0) {
-                        base = "http://" + base;
-                    }
-                    if (!isValidURL(href)) {
-                        console.log("isnt valid url, new url is", base + "/" + href);
-                        href = base + "/" + href;
-                    }
-                    if (href.indexOf("./") === 0) {
-                        href = base + "/" + href.substring(2);
-                    }
+                if (href !== undefined) {
+                  if (href.indexOf(".css") > -1) {
+                      parts = PARSE_URL.exec(url);
+                      base = returnBase(parts);
+                      if (base.indexOf("http") !== 0) {
+                          base = "http://" + base;
+                      }
+                      if (!isValidURL(href)) {
+                          console.log("isnt valid url, new url is", base + "/" + href);
+                          href = base + "/" + href;
+                      }
+                      if (href.indexOf("./") === 0) {
+                          href = base + "/" + href.substring(2);
+                      }
 
-                    // yay its css
-                    request.get(href, function (error, response, body) {
-                        console.log("error downloading:", error);
-                        if (!error && response.statusCode == 200) {
-                            style = body;
-                            // Continue with your processing here.
-                        } else {
-                            style = "";
-                        }
-                        $('head').append("\n<style>\n" + style + "\n</style>\n");
-                        console.log("appended");
-                        $(link).remove();
-                        processLink(j + 1);
-                    });
+                      // yay its css
+                      request.get(href, function (error, response, body) {
+                          console.log("error downloading:", error);
+                          if (!error && response.statusCode == 200) {
+                              style = body;
+                              // Continue with your processing here.
+                          } else {
+                              style = "";
+                          }
+                          $('head').append("\n<style>\n" + style + "\n</style>\n");
+                          console.log("appended");
+                          $(link).remove();
+                          processLink(j + 1);
+                      });
+                    } else {
+                      processLink(j+1);
+                    }
                 } else {
                     $(link).remove();
                     processLink(j + 1);
@@ -125,40 +129,46 @@ cleaner.clean = function (html, url, cb) {
         src = $(elem).attr('src')
         console.log(i, src);
 
-        if (!isValidURL(src)) {
-            parts = PARSE_URL.exec(url);
-            base = returnBase(parts);
-            if (base.indexOf("http") !== 0) {
-                base = "http://" + base;
-            }
-            console.log("isnt valid url, new url is", base + "/" + src);
-            src = base + "/" + src;
+        if (src !== undefined) {
+
+          if (!isValidURL(src)) {
+              parts = PARSE_URL.exec(url);
+              base = returnBase(parts);
+              if (base.indexOf("http") !== 0) {
+                  base = "http://" + base;
+              }
+              console.log("isnt valid url, new url is", base + "/" + src);
+              src = base + "/" + src;
+          }
+
+          if (src.indexOf("//") === 0) {
+              src = "http:" + src;
+          }
+
+          file = "image-" + src.split("/").pop();
+
+          download(src, file, function(type){
+              if (!type) {
+                  processImage(i+1);
+              } else {
+                  imagemin([file], 'build/images', {
+                      plugins: [
+                          imageminMozjpeg({quality: 10}),
+                          imageminPngquant({quality: '0-20', speed: 10})
+                      ]
+                  }).then(files => {
+                      console.log("compressed image", type);
+                      pref = "data:" + type + ";base64,";
+                      imData = files[0]['data'].toString('base64');
+                      $(elem).attr('src', pref + imData);
+                      processImage(i+1);
+                  });
+              }
+          });
+        } else {
+          $(elem).attr('src') = "";
+          processImage(i+1);
         }
-
-        if (src.indexOf("//") === 0) {
-            src = "http:" + src;
-        }
-
-        file = "image-" + src.split("/").pop();
-
-        download(src, file, function(type){
-            if (!type) {
-                processImage(i+1);
-            } else {
-                imagemin([file], 'build/images', {
-                    plugins: [
-                        imageminMozjpeg({quality: 10}),
-                        imageminPngquant({quality: '0-20', speed: 10})
-                    ]
-                }).then(files => {
-                    console.log("compressed image", type);
-                    pref = "data:" + type + ";base64,";
-                    imData = files[0]['data'].toString('base64');
-                    $(elem).attr('src', pref + imData);
-                    processImage(i+1);
-                });
-            }
-        });
     }
     processImage(0);    
 }
