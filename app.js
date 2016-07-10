5a6a04e24667d23977bfd4945dbd4e31b1e6db6b
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-07-09 21:29:35 -0700

    set up app

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+// APP //
+var app = express();
+var port = '5000';
+
+// VIEW ENGINE //
+app.set('view engine', 'html');
+app.engine('html', function(path, options, callback) {
+    fs.readFile(path, 'utf-8', callback);
+});
+
+// MIDDLEWARE //
+app.use(morgan('dev')); // logger
+app.use(express.static(__dirname + '/../client')); // set static folder
+app.use(bodyParser.json()); // parse json
+app.use(bodyParser.urlencoded({ extended: true })); // parse forms
+
+// ROUTES //
+app.get('/', function (req, res) {
+    res.send('hi');
+});
+
+// RUN APP //
+app.use(function(err, req, res, next) { res.status(err.status || 500); });
+app.listen(port, function() {
+  console.log("running on port:" + port);
+});
