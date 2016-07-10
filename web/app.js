ca5a85aba3c3b7c02037f14bfa7ee6cd0839a65c
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-07-09 22:07:43 -0700

    url in url

--- web/app.js
+++ web/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var path = require('path');
 
 // PROCESSOR //
 var model = require('./lib/model');
@@ -20,13 +21,18 @@ app.engine('html', function(path, options, callback) {
 
 // MIDDLEWARE //
 app.use(morgan('dev')); // logger
-app.use(express.static(__dirname + '/../client')); // set static folder
+app.use(express.static(__dirname + '/client')); // set static folder
 app.use(bodyParser.json()); // parse json
 app.use(bodyParser.urlencoded({ extended: true })); // parse forms
 
 // ROUTES //
-app.get('/:url', function (req, res) {
-    res.send(req.params.url);
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/views/index.html'));
+});
+
+app.get('/*', function (req, res) {
+    var url = req.path.substring(1);
+    res.send(url);
 });
 
 // RUN APP //
