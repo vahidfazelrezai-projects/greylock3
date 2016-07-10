39eb58d9344b2b010e0b190f1994f17f127c5bfe
kimberli <kimberlizhong@gmail.com>
2016-07-10 09:03:31 -0700

    use zombie

--- web/lib/model.js
+++ web/lib/model.js
@@ -1,45 +1,46 @@
-// const Browser = require('zombie');
-// var model = {};
-
-// var browser = new Browser({
-//   maxWait: 10000,
-//   silent: true
-// });
-
-// model.open = function (url, cb) {
-//   console.log(browser);
-//   if (['http', 'https'].indexOf(url.split(':')[0]) === -1) {
-//     url = 'http://' + url;
-//   }
-//   browser.visit(url, function() {
-//     if (browser.error) {}
-//     browser.wait(function() {
-//       if (browser.error) {}
-//       cb(browser.html());
-//     });
-//   })
-// }
-
-// module.exports = model;
-
+const Browser = require('zombie');
 var model = {};
-var Horseman = require('node-horseman');
 
-model.open = function(url, cb) {
-  var horseman = new Horseman();
+var browser = new Browser({
+  maxWait: 10000,
+  silent: true
+});
+
+model.open = function (url, cb) {
+  console.log(browser);
   if (['http', 'https'].indexOf(url.split(':')[0]) === -1) {
     url = 'http://' + url;
   }
-  horseman
-    .open(url)
-    .html()
-    .then(function(html){
-      cb(html); 
-    })
-    .finally(function(){
-      return horseman.close();
-    })
-    
+  browser.visit(url, function() {
+    if (browser.error) {}
+    browser.wait(function() {
+      if (browser.error) {}
+      cb(browser.html());
+    });
+  })
 }
 
 module.exports = model;
+
+// var model = {};
+// var Horseman = require('node-horseman');
+
+// model.open = function(url, cb) {
+//   var horseman = new Horseman();
+//   if (['http', 'https'].indexOf(url.split(':')[0]) === -1) {
+//     url = 'http://' + url;
+//   }
+//   horseman
+//     .open(url)
+//     .html()
+//     .then(function(html){
+//       cb(html); 
+//     })
+//     .catch(err, function(err) {})
+//     .finally(function(){
+//       return horseman.close();
+//     })
+    
+// }
+
+// module.exports = model;
