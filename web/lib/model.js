2eea3a666f27bbfd0f6b1a77e8f426c30d95c87a
kimberli <kimberlizhong@gmail.com>
2016-07-10 00:33:17 -0700

    errorrssss

--- web/lib/model.js
+++ web/lib/model.js
@@ -1,18 +1,22 @@
 const Browser = require('zombie');
 var model = {};
 
-var browser = new Browser();
+var browser = new Browser({
+  maxWait: 10000,
+  silent: true
+});
 
 model.open = function (url, cb) {
   if (['http', 'https'].indexOf(url.split(':')[0]) === -1) {
     url = 'http://' + url;
   }
-  browser.visit(url, function(err) {
-    if (err) {} // suppress errors from websites' code
-    browser.wait().then( function() {
+  browser.visit(url, function() {
+    if (browser.error) {}
+    browser.wait(function() {
+      if (browser.error) {}
       cb(browser.html());
     });
-  });
+  })
 }
 
 module.exports = model;
