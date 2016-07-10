3131cc51eb7803feec5e0c17c92f2b81367d7d80
akshayravikumar <akshayr@mit.edu>
2016-07-09 23:34:30 -0700

    regex n chill finally smh

---
+++ web/client/lighten.js
@@ -0,0 +1,73 @@
+window.onload = function() {
+  console.log("smh");
+}
+
+PARSE_URL = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+
+window.onclick = function(e){
+
+    returnBase = function(parts) {
+        ret = "";
+        if (parts[1] !== undefined) {
+          ret += parts[1] + ":" + parts[2];
+        }
+        ret += parts[3];
+        if (parts[4] !== undefined) {
+          ret += ':' + parts[4];
+        } 
+        return ret;
+    }
+
+    e = e || event;
+    var link = findParent('a', e.target || e.srcElement);
+    if (link){
+        if (e.target.href.indexOf("#") > -1) {
+          return;
+        }
+
+        e.preventDefault();
+        base = window.location.hostname;
+
+        // base is the current host of the servicce
+        parts = PARSE_URL.exec(window.location.href);
+        base = returnBase(parts);
+       
+        // currentHost is the base of the actual host
+        targetLocation = parts[5];
+        parts = PARSE_URL.exec(targetLocation);
+        currentHost = returnBase(parts);
+
+        // targetHost is the base of the target link
+        parts = PARSE_URL.exec(e.target);
+        targetHost = returnBase(parts);
+        targetPath = parts[5];
+
+        if (parts[6] !== undefined) {
+          targetPath += "?" + parts[6];
+        }
+        if (parts[7] !== undefined) {
+          targetPath += "#" + parts[7];
+        }
+
+        if (targetHost === base) {
+          newLocation = base + "/" + currentHost + "/" + targetPath;
+        } else {
+          newLocation = base + "/" + e.target.href;
+        }
+
+        window.location.href = newLocation;
+    }
+}
+
+//find first parent with tagName [tagname]
+function findParent(tagname,el){
+    if ((el.nodeName || el.tagName).toLowerCase() === tagname.toLowerCase()){
+        return el;
+    }
+    while (el = el.parentNode){
+        if ((el.nodeName || el.tagName).toLowerCase() === tagname.toLowerCase()){
+            return el;
+        }
+    }
+    return null;
+}
--- web/lib/cleaner.js
+++ web/lib/cleaner.js
@@ -1,10 +1,20 @@
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+fs = require('fs');
+path = require('path');
+
 var cleaner = {};
 
 cleaner.clean = function (html, cb) {
   var $ = cheerio.load(html);
-  $('img').remove()
-  cb($.html());
+  $('img').remove();
+
+  fs.readFile(path.resolve(__dirname, '../client/lighten.js'), 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    $('head').append("\n<script>\n" + data + "\n</script>\n")
+    cb($.html());
+  });
 }
 
 module.exports = cleaner;
